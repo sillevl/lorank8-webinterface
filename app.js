f03b4fcb7da7870456ebae7fbe376df33ad75a34
Sille Van Landschoot <info@sillevl.be>
2016-10-07 22:40:23 +0200

    add handlebars views to project

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
+
 var app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.render('home');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(3000);
