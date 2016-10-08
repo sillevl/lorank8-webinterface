4e477ae2b5b0dcba5520078f8d48096306e60ef5
Sille Van Landschoot <info@sillevl.be>
2016-10-08 19:49:20 +0200

    add static directory in express for js and css files

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use('/static', express.static('public'));
+
 app.get('/', function (req, res) {
     res.render('home');
 });
